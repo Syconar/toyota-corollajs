9e49fc9ee4272d8a3133698994a90254cc9789a1
Syconar <urbasekfilip@gmail.com>
2025-08-11 09:22:27 +0200

    Car and assets added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useGLTF, PresentationControls } from "@react-three/drei";
+import { useRef } from "react";
+
+function Model(props){
+  const { scene } = useGLTF("/toyota_corolla_ps1.glb");
+  const ref = useRef();
+
+  // Rotate the model on every frame
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += -0.01;
+    }
+  });
+
+  return <primitive ref={ref} object={scene} {...props} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      dpr={[1.2]}
+      shadows 
+      camera={{ fov: 45, position: [0, 1, 5] }} 
+      style={{"position": "absolute", /* width: "500px", height: "500px" */}}>
+      <color attach="background" args={["#101010"]} />
+
+      <ambientLight intensity={2}/>
+
+      <PresentationControls 
+        speed={2} 
+        global 
+        zoom={1} 
+        polar={[0, Math.PI / 4]}>
+          <Model scale={0.2}/>
+      </PresentationControls>
+      
+    </Canvas>
   );
 }
 
